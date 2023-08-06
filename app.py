a18cd3d39687fc72a98b5c24edca01ad9b6720f1
Dark36555 <134802061+Dark36555@users.noreply.github.com>
2023-08-06 19:56:01 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,23 @@
 from flask import Flask, request, jsonify
 app = Flask(__name__)
 
+# ... (your existing Flask code) ...
+
+@app.route('/quiz/evaluate', methods=['POST'])
+def evaluate_quiz():
+    user_answers = request.form
+
+    total_score = 0
+    for question_number, correct_answer_index in enumerate(quiz_questions):
+        user_answer = user_answers.get(f'q{question_number + 1}')
+        if user_answer == quiz_questions[question_number]['options'][correct_answer_index]:
+            total_score += 1
+
+    return f"You scored {total_score} out of {len(quiz_questions)}."
+
+# ... (the rest of your Flask code) ...
+
+
 # Sample quiz questions
 quiz_questions = [
     {
